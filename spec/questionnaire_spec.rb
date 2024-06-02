3250332e56485c06b87129db8b389e1faf19c2f9
dplekurwale <42389620+dplekurwale@users.noreply.github.com>
2024-06-02 15:47:38 +0530

    Update questionnaire_spec.rb

--- spec/questionnaire_spec.rb
+++ spec/questionnaire_spec.rb
@@ -1,87 +1,102 @@
 require 'rspec'
 require 'pstore'
-require_relative '../questionnaire'
+require 'byebug'
+require_relative '../questionnaire' # Adjust path as necessary
 
-RSpec.describe Questionnaire do
+RSpec.describe Answer do
+  it 'initializes with a value and score' do
+    answer = Answer.new('y', 100)
+    expect(answer.value).to eq('y')
+    expect(answer.score).to eq(100)
+  end
+end
+
+RSpec.describe Question do
+  let(:options) { [Answer.new('y', 100), Answer.new('n', 0), Answer.new('yes', 100), Answer.new('no', 0)] }
+  let(:question) { Question.new('q1', 'Can you code in Ruby?', options) }
+
+  it 'initializes with an id, statement, and options' do
+    expect(question.instance_variable_get(:@id)).to eq('q1')
+    expect(question.statement).to eq('Can you code in Ruby?')
+    expect(question.options.length).to eq(4)
+  end
+
+  it 'validates input correctly' do
+    expect(question.valid_input?('y')).to be true
+    expect(question.valid_input?('yes')).to be true
+    expect(question.valid_input?('maybe')).to be false
+  end
+
+  it 'returns the correct score based on the answer' do
+    expect(question.score('y')).to eq(100)
+    expect(question.score('n')). to eq(0)
+  end
+end
+
+RSpec.describe Survey do
   let(:store_name) { 'test_store.pstore' }
-  let(:survey) { Questionnaire.new }
+  let(:questions) do
+    QUESTIONS.map do |k, v|
+      options = [Answer.new('y', 100), Answer.new('n', 0), Answer.new('yes', 100), Answer.new('no', 0)]
+      Question.new(k, v, options)
+    end
+  end
+  let(:survey) { Survey.new(questions) }
 
   before(:each) do
     @store = PStore.new(store_name)
-    allow_any_instance_of(Questionnaire).to receive(:gets).and_return('yes') # Default stub to prevent hangs
     survey.instance_variable_set(:@store, @store)
+    survey.clear_store # Clear store to ensure clean state for tests
   end
 
-  after(:each) do
-    File.delete(store_name) if File.exist?(store_name)
+  it 'initializes with questions and score' do
+    expect(survey.score).to eq(0)
+    expect(survey.questions).to eq(questions)
   end
 
-  describe '#valid_input?' do
-    it 'returns true for valid inputs' do
-      %w[yes y no n].each do |answer|
-        expect(survey.send(:valid_input?, answer)).to be true
-      end
-    end
-
-    it 'returns false for invalid inputs' do
-      expect(survey.send(:valid_input?, 'invalid')).to be false
-    end
+  it 'returns the correct question count' do
+    expect(survey.question_count).to eq(5)
   end
 
-  describe '#do_prompt' do
-    it 'prompts the user for each question and stores answers' do
-      allow_any_instance_of(Questionnaire).to receive(:gets).and_return('yes', 'no', 'yes', 'no', 'no')
-      expect { survey.do_prompt }.to output(/Can you code in Ruby?.*Can you code in JavaScript?.*Can you code in Swift?.*Can you code in Java?.*Can you code in C#?/m).to_stdout
-
-      stored_answers = nil
-      @store.transaction { stored_answers = @store[:answers] }
-
-      expect(stored_answers).not_to be_nil
-      expect(stored_answers.last).to eq({ 'q1' => 'yes', 'q2' => 'no', 'q3' => 'yes', 'q4' => 'no', 'q5' => 'no' })
-    end
-
-    it 'calculates and prints the rating for the current run' do
-      allow_any_instance_of(Questionnaire).to receive(:gets).and_return('yes', 'yes', 'no', 'yes', 'no')
-      expect { survey.do_prompt }.to output(/Rating for this run: 60.0%/).to_stdout
-    end
+  it 'returns an average rating of 0 with no runs' do
+    expect(survey.average_rating).to eq(0.0)
   end
 
-  describe '#calculate_rating' do
-    it 'returns the correct rating' do
-      expect(survey.send(:calculate_rating, 3, 5)).to eq(60.0)
+  it 'saves score in the database' do
+    survey.instance_variable_set(:@score, 400) # Set a dummy score
+    survey.save_in_db
+
+    scores = []
+    survey.instance_variable_get(:@store).transaction do
+      scores = survey.instance_variable_get(:@store)[:scores]
     end
+    expect(scores).to eq([400])
   end
 
-  describe '#calculate_average_rating' do
-    it 'returns the correct average rating' do
-      allow_any_instance_of(Questionnaire).to receive(:gets).and_return('yes', 'no', 'no', 'yes', 'no')
-      survey.do_prompt
-
-      allow_any_instance_of(Questionnaire).to receive(:gets).and_return('no', 'no', 'no', 'no', 'no')
-      survey.do_prompt
-
-      expect(survey.send(:calculate_average_rating)).to eq(20.0)
-    end
+  it 'returns the correct average rating' do
+    survey.instance_variable_set(:@score, 400) # Set a dummy score
+    survey.save_in_db
+    expect(survey.average_rating).to eq(80.0)
   end
 
-  describe '#do_report' do
-    it 'calculates and prints the average rating for all runs' do
-      allow_any_instance_of(Questionnaire).to receive(:gets).and_return('yes', 'yes', 'yes', 'yes', 'no')
-      survey.do_prompt
-      allow_any_instance_of(Questionnaire).to receive(:gets).and_return('no', 'no', 'no', 'no', 'no')
-      survey.do_prompt
-      expect { survey.do_report }.to output(/Average rating for all runs: 40.0%/).to_stdout
+  it 'clears the store' do
+    survey.instance_variable_set(:@score, 400) # Set a dummy score
+    survey.save_in_db
+    survey.clear_store
+
+    scores = []
+    survey.instance_variable_get(:@store).transaction do
+      scores = survey.instance_variable_get(:@store)[:scores]
     end
+    expect(scores).to eq([])
   end
 
-  describe '#clear_store_answers' do
-    it 'clears all stored answers' do
-      allow_any_instance_of(Questionnaire).to receive(:gets).and_return('yes', 'yes', 'no', 'yes', 'no')
-      survey.do_prompt
-      survey.send(:clear_store_answers)
-      stored_answers = nil
-      @store.transaction { stored_answers = @store[:answers] }
-      expect(stored_answers).to eq([])
-    end
+  it 'completes the survey' do
+    # Mock user input for each question
+    allow(survey).to receive(:gets).and_return('y', 'n', 'yes', 'no', 'y')
+
+    # Expect the survey to run without errors and check the output
+    expect { survey.do_survey }.to output(/Rating for this run:/).to_stdout
+    expect(survey.score).to eq(300)
   end
 end
